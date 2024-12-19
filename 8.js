da9f886038d664f56dcecaa754b5c404fb782d0d
Carlos <carlos1.teste2024@gmail.com>
2024-12-19 15:49:22 -0300

    atualização

--- 8.js
+++ 8.js
@@ -3,19 +3,19 @@ const prato = prompt("Escolha um prato: 1 - Pizza\ 2 - Hambúrguer\ 3 - Salada\ 
 
 switch (prato) {
 case "1":
-        alert("Pizza: R$ 35,00 Massa crocante com ou sem queijo, Catupirí, Calabresa, Frango ou Mista");
+        console.log("Pizza: R$ 35,00 Massa crocante com ou sem queijo, Catupirí, Calabresa, Frango ou Mista");
         break;
     case "2": 
-        alert("Hamburguer: R$ 15,00 X-tudo, X-salada ou X-bacon");
+        console.lg("Hamburguer: R$ 15,00 X-tudo, X-salada ou X-bacon");
         break;
     case "3":
-        alert("Salada: R$ 14,00 alface, tomate, cecola, pepino e pimentão");
+        console.log("Salada: R$ 14,00 alface, tomate, cecola, pepino e pimentão");
         break;
     case "4":
-        alert("Macarrão: R$ 12,00 passado ao azeite com calabresa, queijo e molho da sua preferencia");
+        console.log("Macarrão: R$ 12,00 passado ao azeite com calabresa, queijo e molho da sua preferencia");
          break;
     default:
-        alert("Opção invalida");
+        console.log("Opção invalida");
         break;
 
 }
